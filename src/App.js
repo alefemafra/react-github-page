ae34584dac54c7e299c12149e82eec451dec266d
Alefe <emanoelmafra@gmail.com>
2019-09-09 10:17:07 -0300

    dziuba

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dziuba Viadão que da o bundão
         </p>
         <a
           className="App-link"
